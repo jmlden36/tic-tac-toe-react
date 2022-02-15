e38b4758d191062c4c056f0ef4541555a62ef930
Matt Luker <jamesmattluker@gmail.com>
2022-02-15 12:40:34 -0700

    add a draw counter to Board props and a draw message
    Co-authored-by: Katie Pundt <kzpundt@gmail.com>

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,12 +1,51 @@
 import React from "react";
 import Square from "./Square"
+
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+      counter: 0,
+    };
+  }
+  
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+      counter: this.state.counter+1
+    });
+    console.log(this.state.counter);
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square 
+        value={this.state.squares[i]} 
+        onClick={() => this.handleClick(i)}  
+      />
+    );
   }
 
   render() {
-    const status = 'Next player: X';
+    console.log(calculateWinner(this.state.squares, this.state.counter))
+    const winner = calculateWinner(this.state.squares, this.state.counter);
+    let status;
+    if (winner === undefined) {
+      status = "Draw";
+    } else if (winner) {
+      status = "Winner: " + winner;
+    
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -29,6 +68,29 @@ class Board extends React.Component {
       </div>
     )
   }
+}
+
+function calculateWinner(squares, counter) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (counter === 9 && !(squares[a] && squares[a] === squares[b] && squares[a] === squares[c])) {
+      return;
+    } else if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 export default Board;
